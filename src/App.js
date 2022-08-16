b352e6c5703e178bbec1bcc2d3d7fce3f550e2a1
pikkilisaikumar <pikkilisaikumar@gmail.com>
2022-08-16 15:53:08 +0530

    project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="normal__container">
+      <div className="box__container">
+        <span className="span">span x</span>
+        <h1 className="heading">Heading One</h1>
+        <button className="button">Submit</button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
